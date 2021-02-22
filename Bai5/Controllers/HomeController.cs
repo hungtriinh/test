0d69f4667d5c0acf4e8fa5579b3a98e17aa88074
hunghoang910 <hungtrhoang910@gmail.com>
2021-02-22 23:02:06 +0700

    Version 1.0
    Upload the first version

---
+++ Bai5/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            string text1 = Request["text1"];
+            string text2 = Request["text2"];
+
+            Response.Cookies["ckText1"].Value = text1;
+            Response.Cookies["ckText1"].Expires = DateTime.Now.AddMinutes(1);
+            Response.Cookies["ckText2"].Value = text2;
+            Response.Cookies["ckText2"].Expires = DateTime.Now.AddMinutes(1);
+
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            string ck1 = Request.Cookies["ckText1"].Value;
+            string ck2 = Request.Cookies["ckText2"].Value;
+            ViewBag.text1 = ck1;
+            ViewBag.text2 = ck2;
+            return View();
+        }
+    }
+}
